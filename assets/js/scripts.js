833b0cad0077c0e1465a1766d4265a1b2614beeb
Mike Crane <mike.c0321@gmail.com>
2016-03-06 20:12:18 -0500

    2nd attempt at running JS to board

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+// going about this a different way
+// need to incorporate x-axis & y-axis in order to run game play
+
+function print(){
+   "use strict";
+
+    var rankX = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    document.write('<table>');
+    var index = 0;
+
+	console.log(rank);
+
+}
+
+    //    .each(function(rank, key) {
+    //     document.write("<tr>");
+    //     var fileY = 1;
+	//
+    //    .each(function(box, k) {
+    //     var boxId = rankX[index] + fileY;
+    //     document.write("<td id=" + boxId + ">" + box + "</td>")
+    //     fileY++;
+	//
+    // });
