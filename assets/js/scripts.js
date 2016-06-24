57086abbff2a18bd190fac054312fbf2f17a4cc8
Mike Crane <mike.c0321@gmail.com>
2016-06-24 11:56:08 -0400

    clean up dead files

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
     'use strict';
 
-// declare variables for each chess piece using unicode
-/* black king   */   var K = "&#9818";
-/* white king   */   var k = "&#9812";
-/* black queen  */   var Q = "&#9819";
-/* white queen  */   var q = "&#9813";
-/* black bishop */   var B = "&#9821";
-/* white bishop */   var b = "&#9815";
-/* black knight */   var N = "&#9822";
-/* white knight */   var n = "&#9816";
-/* black rook   */   var R = "&#9820";
-/* white rook   */   var r = "&#9814";
-/* black pawn   */   var P = "&#9823";
-/* white pawn   */   var p = "&#9817";
+    // declare variables for each chess piece using unicode
+
+    var K = "&#9818"; /*    black king    */
+    var k = "&#9812"; /*    white king    */
+    var Q = "&#9819"; /*   black queen    */
+    var q = "&#9813"; /*   white queen    */
+    var B = "&#9821"; /*   black bishop   */
+    var b = "&#9815"; /*   white bishop   */
+    var N = "&#9822"; /*   black knight   */
+    var n = "&#9816"; /*   white knight   */
+    var R = "&#9820"; /*    black rook    */
+    var r = "&#9814"; /*    white rook    */
+    var P = "&#9823"; /*    black pawn    */
+    var p = "&#9817"; /*    white pawn    */
 
 
-// multi-dimensional arrays representing the location the pieces along each sequence of moves
+    // multi-dimensional arrays representing the location of the pieces along each sequence of moves
     var moveSequence = [
-        [																							// Initial board setup
+        [ // Initial board setup
             [R, N, B, Q, K, B, N, R],
             [P, P, P, P, P, P, P, P],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -28,7 +29,7 @@ $(document).ready(function() {
             [p, p, p, p, p, p, p, p],
             [r, n, b, q, k, b, n, r]
         ],
-        [																							// Move sequence #1
+        [ // Move sequence #1
             [R, N, B, Q, K, B, N, R],
             [P, P, P, P, ' ', P, P, P],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -38,7 +39,7 @@ $(document).ready(function() {
             [p, p, p, p, p, p, p, p],
             [r, n, b, q, k, b, n, r]
         ],
-        [																							// Move sequence #2
+        [ // Move sequence #2
             [R, N, B, Q, K, B, N, R],
             [P, P, P, P, ' ', P, P, P],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -48,7 +49,7 @@ $(document).ready(function() {
             [p, p, p, p, p, p, p, p],
             [r, ' ', b, q, k, b, n, r]
         ],
-        [																							// Move sequence #3
+        [ // Move sequence #3
             [R, N, B, Q, K, B, N, R],
             [P, P, P, P, ' ', ' ', P, P],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -58,7 +59,7 @@ $(document).ready(function() {
             [p, p, p, p, p, p, p, p],
             [r, ' ', b, q, k, b, n, r]
         ],
-        [																							// Move sequence #4
+        [ // Move sequence #4
             [R, N, B, Q, K, B, N, R],
             [P, P, P, P, ' ', ' ', P, P],
             [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
@@ -68,7 +69,7 @@ $(document).ready(function() {
             [p, p, p, ' ', p, p, p, p],
             [r, ' ', b, q, k, b, n, r]
         ],
-        [																							// Move sequence #5
+        [ // Move sequence #5
             [R, N, B, Q, K, B, N, R],
             [P, ' ', P, P, ' ', ' ', P, P],
             [' ', P, ' ', ' ', ' ', ' ', ' ', ' '],
@@ -78,7 +79,7 @@ $(document).ready(function() {
             [p, p, p, ' ', p, p, p, p],
             [r, ' ', b, q, k, b, n, r]
         ],
-        [																							// Move sequence #6
+        [ // Move sequence #6
             [R, N, B, Q, K, B, N, R],
             [P, ' ', P, P, ' ', ' ', P, P],
             [' ', P, ' ', ' ', ' ', ' ', ' ', ' '],
@@ -88,7 +89,7 @@ $(document).ready(function() {
             [p, p, p, ' ', ' ', p, p, p],
             [r, ' ', b, q, k, b, n, r]
         ],
-        [																							// Move sequence #7
+        [ // Move sequence #7
             [R, N, ' ', Q, K, B, N, R],
             [P, B, P, P, ' ', ' ', P, P],
             [' ', P, ' ', ' ', ' ', ' ', ' ', ' '],
@@ -98,7 +99,7 @@ $(document).ready(function() {
             [p, p, p, ' ', ' ', p, p, p],
             [r, ' ', b, q, k, b, n, r]
         ],
-        [																							// Move sequence #8
+        [ // Move sequence #8
             [R, N, ' ', Q, K, B, N, R],
             [P, B, P, P, ' ', ' ', P, P],
             [' ', P, ' ', ' ', ' ', ' ', ' ', ' '],
@@ -108,7 +109,7 @@ $(document).ready(function() {
             [p, p, p, b, ' ', p, p, p],
             [r, ' ', ' ', q, k, b, n, r]
         ],
-        [																							// Move sequence #9
+        [ // Move sequence #9
             [R, ' ', ' ', Q, K, B, N, R],
             [P, B, P, P, ' ', ' ', P, P],
             [' ', P, N, ' ', ' ', ' ', ' ', ' '],
@@ -121,85 +122,96 @@ $(document).ready(function() {
     ];
 
 
-// keeps a count on the current move sequence
+    // keeps a count on the current move sequence
     var counter = 0;
 
-// sets the board with initial piece positions
+    // sets the board with initial piece positions
     for (var i = 0; i < 8; i++) {
         for (var f = 0; f < 8; f++) {
 
-// creates the chess board of alternating squares
-            document.getElementById("boardSet").innerHTML += "<div class='grid-box'>" + moveSequence[counter][i][f] + "</div>";
+            // creates the chess board of alternating squares
+            document.getElementById("boardSet").innerHTML +=
+                "<div class='grid-box'>" + moveSequence[counter][i][f] +
+                "</div>";
 
-            if (i % 2 === 0 && f % 2 !== 0 || i % 2 !== 0 && f % 2 === 0) {
+            if (i % 2 === 0 && f % 2 !== 0 || i % 2 !== 0 && f % 2 ===
+                0) {
 
-// provides a unique [X & Y] coordinate for each square
+                // provides a unique [X & Y] coordinate for each square
                 $(".grid-box").eq(i * 8 + f).addClass("grid-box-2");
 
             } else {}
         }
     }
 
-    document.querySelector("#back").addEventListener("click", function() {
+    document.querySelector("#back").addEventListener("click",
+        function() {
 
-// drops back one move sequence by subtracting 1 from the count
-        if (counter > 0) {
-            counter -= 1;
-        } else {}
+            // drops back one move sequence by subtracting 1 from the count
+            if (counter > 0) {
+                counter -= 1;
+            } else {}
 
-        console.log(counter);
+            console.log(counter);
 
-// clears the board to allow room for the next move sequence
-        document.getElementsByClassName('grid-box').innerHTML = null;
-
-        for (var i = 0; i < 8; i++) {
-            for (var f = 0; f < 8; f++) {
-
-// sets the board with the piece positions of the current move sequence
-                $(".grid-box").eq(i * 8 + f).html(moveSequence[counter][i][f]);
-            }
-        }
-    });
-
-// play from the current move sequence to the last move sequence
-    document.querySelector("#play").addEventListener("click", function() {
-
-        var initializeGame = setInterval(function() {
-            if (counter < moveSequence.length - 1) {
-                counter += 1;
-            } else {
-// signals a stop after the last move sequence
-                clearInterval(initializeGame);
-            }
+            // clears the board to allow room for the next move sequence
+            document.getElementsByClassName('grid-box').innerHTML =
+                null;
 
             for (var i = 0; i < 8; i++) {
                 for (var f = 0; f < 8; f++) {
 
-// sets the board with the piece positions of the current move sequence
-                    $(".grid-box").eq(i * 8 + f).html(moveSequence[counter][i][f]);
+                    // sets the board with the piece positions of the current move sequence
+                    $(".grid-box").eq(i * 8 + f).html(moveSequence[
+                        counter][i][f]);
                 }
             }
-        }, 600);
-    });
+        });
 
-// advances forward one move sequence by adding 1 to the count
-    document.querySelector("#forward").addEventListener("click", function() {
+    // play from the current move sequence to the last move sequence
+    document.querySelector("#play").addEventListener("click",
+        function() {
 
-        if (counter < moveSequence.length - 1) {
-            counter += 1;
-        } else {}
+            var initializeGame = setInterval(function() {
+                if (counter < moveSequence.length - 1) {
+                    counter += 1;
+                } else {
+                    // signals a stop after the last move sequence
+                    clearInterval(initializeGame);
+                }
 
-        console.log(counter);
+                for (var i = 0; i < 8; i++) {
+                    for (var f = 0; f < 8; f++) {
 
-// clears the board to allow room for the next move sequence
-        document.getElementsByClassName('grid-box').innerHTML = null;
+                        // sets the board with the piece positions of the current move sequence
+                        $(".grid-box").eq(i * 8 + f).html(moveSequence[
+                            counter][i][f]);
+                    }
+                }
+            }, 600);
+        });
 
-        for (var i = 0; i < 8; i++) {
-            for (var f = 0; f < 8; f++) {
+    // advances forward one move sequence by adding 1 to the count
+    document.querySelector("#forward").addEventListener("click",
+        function() {
 
-// sets the board with the piece positions of the current move sequence
-                $(".grid-box").eq(i * 8 + f).html(moveSequence[counter][i][f]);
+            if (counter < moveSequence.length - 1) {
+                counter += 1;
+            } else {}
+
+            console.log(counter);
+
+            // clears the board to allow room for the next move sequence
+            document.getElementsByClassName('grid-box').innerHTML =
+                null;
+
+            for (var i = 0; i < 8; i++) {
+                for (var f = 0; f < 8; f++) {
+
+                    // sets the board with the piece positions of the current move sequence
+                    $(".grid-box").eq(i * 8 + f).html(moveSequence[
+                        counter][i][f]);
+                }
             }
-        }
-    });
+        });
 });
